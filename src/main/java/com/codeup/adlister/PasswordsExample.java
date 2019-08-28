56e246d44840418af83a3e9a471d585a6f5d6ee3
Fernando Mendoza <fernandoza@gmail.com>
2019-08-28 11:15:33 -0500

    feat: small example of the use of BCrypt

---
+++ src/main/java/com/codeup/adlister/PasswordsExample.java
@@ -0,0 +1,16 @@
+package com.codeup.adlister;
+
+import org.mindrot.jbcrypt.BCrypt;
+
+public class PasswordsExample {
+
+    public static void main(String[] args) {
+        String password = "password123";
+        String hash = BCrypt.hashpw(password, BCrypt.gensalt());
+        System.out.println("password = " + password);
+        System.out.println("hash = " + hash);
+
+        System.out.println("match = " + BCrypt.checkpw(password, hash));
+
+    }
+}
