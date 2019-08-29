8bf856747acc1f97370489fa3cc3e40f6cdd24ed
Kyle Maulsby <kylemaulsby15@gmail.com>
2019-08-29 11:45:53 -0500

    migration

--- migration.sql
+++ migration.sql
@@ -1,5 +1,7 @@
 USE adlister_db;
 
+DROP TABLE IF EXISTS cat_ads;
+DROP TABLE IF EXISTS categories;
 DROP TABLE IF EXISTS ads;
 DROP TABLE IF EXISTS users;
 
@@ -8,6 +10,9 @@ CREATE TABLE users (
     username VARCHAR(240) NOT NULL,
     email VARCHAR(240) NOT NULL,
     password VARCHAR(255) NOT NULL,
+    full_name VARCHAR(250),
+    address VarChar(250),
+    photo VarChar(350),
     PRIMARY KEY (id)
 );
 
@@ -20,3 +25,40 @@ CREATE TABLE ads (
     FOREIGN KEY (user_id) REFERENCES users(id)
         ON DELETE CASCADE
 );
+
+CREATE TABLE categories (
+     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+     category VARCHAR(100) NOT NULL,
+     PRIMARY KEY (id)
+);
+
+CREATE TABLE cat_ads (
+     ad_id INT UNSIGNED NOT NULL,
+     category_id INT UNSIGNED NOT NULL,
+     FOREIGN KEY (ad_id) REFERENCES ads(id)
+         ON DELETE CASCADE,
+     FOREIGN KEY (category_id) REFERENCES categories(id)
+        ON DELETE CASCADE
+);
+
+INSERT INTO categories(category)
+VALUES ('Appliances'),
+       ('Automotive'),
+       ('Clothes'),
+       ('Electronics'),
+       ('Furniture'),
+       ('General'),
+       ('Jewelry'),
+       ('Tools'),
+       ('Toys');
+
+# CREATE TABLE profile (
+#      id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+#      profile_id INT UNSIGNED NOT NULL,
+#
+#      PRIMARY KEY(id),
+#      FOREIGN KEY (profile_id) REFERENCES users(id)
+#         ON DELETE CASCADE
+# );
+
+
